98877aa4c028ddc2ebc65651385a3aec3e8548fa
wico <wico.wen@dlinkcorp.com>
2016-02-03 19:54:06 +0800

    rename sliderMoves to slideMovied

--- Swift_BullsEye/ViewController.swift
+++ Swift_BullsEye/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,7 +35,7 @@ class ViewController: UIViewController {
     
     }
     
-    @IBAction func sliderMoves(slider: UISlider) {
+    @IBAction func sliderMoved(slider: UISlider) {
         print("The value of the slider is now: \(slider.value)")
     }
     
