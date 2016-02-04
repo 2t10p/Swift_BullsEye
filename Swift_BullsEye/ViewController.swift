0bfe10472f1b47ba294a5aa3f471d19c0a7657e1
wico <wico.wen@dlinkcorp.com>
2016-02-05 01:47:42 +0800

    reset value every clikc button

--- Swift_BullsEye/ViewController.swift
+++ Swift_BullsEye/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue: Int = 0
     
     @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        startNewRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,8 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
     @IBAction func showAlert() {
-        let message = "The value of the slider is : \(currentValue)"
+        
+        let message = "The value of the slider is : \(currentValue)\n"
+                    + "The target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello World",
             message: message,
@@ -39,7 +48,8 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
-    
+
+        startNewRound()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
